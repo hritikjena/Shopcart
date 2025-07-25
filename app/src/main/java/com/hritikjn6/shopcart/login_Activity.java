990bfc9416afa3b14929caf3be3e281f7d8e66fd
Hritik Jena <hritikjn6@gmail.com>
2025-07-25 14:30:08 +0530

    Created a LogIn page (login_Activity.java) with basic functionality

--- app/src/main/java/com/hritikjn6/shopcart/login_Activity.java
+++ app/src/main/java/com/hritikjn6/shopcart/login_Activity.java
@@ -1,6 +1,9 @@
 package com.hritikjn6.shopcart;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,19 @@ public class login_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+        // Handling system UI padding
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.loginLayout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Now use Button instead of SpannableString
+        Button signUpButton = findViewById(R.id.signUpButton);
+        signUpButton.setOnClickListener(view -> {
+            Intent intent = new Intent(login_Activity.this, SignUpActivity.class);
+            startActivity(intent);
         });
     }
 }
